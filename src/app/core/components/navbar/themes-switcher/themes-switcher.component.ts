ea509d7f03408d6ccce7a793f9d83c89a2e36df4
piotrsioch <windyswire@gmail.com>
2023-12-16 17:34:31 +0100

    Add variables and setup path for it in angular.json

--- src/app/core/components/navbar/themes-switcher/themes-switcher.component.ts
+++ src/app/core/components/navbar/themes-switcher/themes-switcher.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'sh-themes-switcher',
   standalone: true,
   templateUrl: './themes-switcher.component.html',
   styleUrls: ['./themes-switcher.component.scss'],
+  imports: [
+    MatIconModule
+  ]
 })
 export class ThemesSwitcherComponent {
   public isLightTheme = false;
