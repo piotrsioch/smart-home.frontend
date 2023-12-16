a80b91762dc2d6d1a5e29b3b7afabf214cb10728
piotrsioch <93688242+piotrsioch@users.noreply.github.com>
2023-12-16 18:41:39 +0100

    Feature/navbar (#3)
    * Add variables and setup path for it in angular.json
    
    * Style navbar
    
    * Code refactor

--- src/app/core/components/navbar/themes-switcher/themes-switcher.component.ts
+++ src/app/core/components/navbar/themes-switcher/themes-switcher.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'sh-themes-switcher',
   standalone: true,
   templateUrl: './themes-switcher.component.html',
   styleUrls: ['./themes-switcher.component.scss'],
+  imports: []
 })
 export class ThemesSwitcherComponent {
   public isLightTheme = false;
