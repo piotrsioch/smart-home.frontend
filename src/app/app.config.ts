3428c88c0de85496a006de633a958aa0032185b7
piotrsioch <windyswire@gmail.com>
2023-12-18 01:05:18 +0100

    Align root layout styling with footer and navbar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig } from "@angular/core";
 import { provideRouter, Routes } from "@angular/router";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { SettingsComponent } from "./components/settings/settings.component";
+import { NotificationsComponent } from "./components/notifications/notifications.component";
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'test', component: SettingsComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'notifications', component: NotificationsComponent },
   { path: '**', redirectTo: 'dashboard' },
 ]
 
