44104d3351abd91d453b39609b51ebb7cae8b022
piotrsioch <windyswire@gmail.com>
2023-12-27 15:10:37 +0100

    Fetch climate data every X seconds

--- src/app/components/climate/climate.component.ts
+++ src/app/components/climate/climate.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoaderComponent } from "../../shared/components/loader/loader.component";
-import { BehaviorSubject, forkJoin, Subscription } from "rxjs";
+import { BehaviorSubject, forkJoin, interval, Observable, startWith, Subscription } from "rxjs";
 import { CommonModule } from "@angular/common";
 import { DhtSensorService } from "../../core/api/services/dht-sensor.service";
 import { SensorsService } from "../../core/api/services/sensors.service";
@@ -20,7 +20,7 @@ import { SmokeSensorService } from "../../core/api/services/smoke-sensor.service
   templateUrl: './climate.component.html',
   styleUrl: './climate.component.scss'
 })
-export class ClimateComponent implements OnDestroy {
+export class ClimateComponent implements OnInit, OnDestroy {
   public dhtSensors: SensorDto[] = [];
   public dhtData: DhtSensorDto[] = [];
   public smokeSensors: SensorDto[] = [];
@@ -39,61 +39,64 @@ export class ClimateComponent implements OnDestroy {
         this.loading = isLoading;
       })
     );
-
-    this.subscription.add(
-      this.sensorsService.sensorControllerSensorList({
-        page: 0,
-        limit: 50,
-        search: 'DHT_'
-      }).pipe(
-        tap(data => {
-          this.loadingSubject.next(true);
-          this.dhtSensors = data.items || [];
-        }),
-        map(data => data.items!),
-        switchMap(sensors => {
-          const observableArray = sensors.map(sensor =>
-            this.dhtService.dhtSensorControllerGetLatestData({
-              sensorId: sensor._id
-            })
-          );
-
-          return forkJoin(observableArray);
-        })
-      ).subscribe(data => {
-        this.dhtData = data;
-        this.loadingSubject.next(false);
-      })
-    );
-
-    this.subscription.add(
-      this.sensorsService.sensorControllerSensorList({
-        search: 'SMOKE_',
-        page: 0,
-        limit: 50,
-      }).pipe(
-        map(data => data.items!),
-        tap(data => {
-          this.smokeSensors = data || [];
-          this.loadingSubject.next(true);
-        }),
-        switchMap(sensors => {
-          const observableArray = sensors.map(sensor => {
-            return this.smokeService.smokeSensorControllerGetLatestData({
-              sensorId: sensor._id,
-            });
-          })
-
-          return forkJoin(observableArray);
-        })
-      ).subscribe(data => {
-        this.smokeData = data;
-        this.loadingSubject.next(false);
-      })
-    )
   }
 
-  ngOnDestroy() {
+  ngOnInit(): void {
+    this.subscription.add(
+      interval(3000).pipe(
+        startWith(0),
+        switchMap(() => this.fetchSensorData())
+      ).subscribe(([dhtData, smokeData]) => {
+          this.dhtData = dhtData;
+          this.smokeData = smokeData;
+          this.loadingSubject.next(false);
+        },
+      )
+    );
+  }
+
+  ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  private fetchSensorData(): Observable<[DhtSensorDto[], SmokeSensorDto[]]> {
+    return forkJoin([
+      this.fetchDhtSensorData(),
+      this.fetchSmokeSensorData()
+    ]);
+  }
+
+  private fetchDhtSensorData(): Observable<DhtSensorDto[]> {
+    return this.sensorsService.sensorControllerSensorList({
+      page: 0,
+      limit: 50,
+      search: 'DHT_'
+    }).pipe(
+      map(data => data.items!),
+      tap(data => this.dhtSensors = data || []),
+      switchMap(sensors => {
+        const observableArray = sensors.map(sensor =>
+          this.dhtService.dhtSensorControllerGetLatestData({sensorId: sensor._id})
+        );
+        return forkJoin(observableArray);
+      })
+    );
+  }
+
+  private fetchSmokeSensorData(): Observable<SmokeSensorDto[]> {
+    return this.sensorsService.sensorControllerSensorList({
+      search: 'SMOKE_',
+      page: 0,
+      limit: 50,
+    }).pipe(
+      map(data => data.items!),
+      tap(data => this.smokeSensors = data || []),
+      switchMap(sensors => {
+        const observableArray = sensors.map(sensor =>
+          this.smokeService.smokeSensorControllerGetLatestData({sensorId: sensor._id})
+        );
+        return forkJoin(observableArray);
+      })
+    );
   }
 }
