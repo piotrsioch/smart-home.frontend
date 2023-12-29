bb9132107110e71c7638697b1cb5119304e015a5
piotrsioch <windyswire@gmail.com>
2023-12-29 01:22:34 +0100

    Add confirm modal

---
+++ src/app/shared/components/modal/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,42 @@
+import { Component, Inject } from '@angular/core';
+import { ModalButtonsComponent } from "../modal-buttons/modal-buttons.component";
+import { ModalHeaderComponent } from "../modal-header/modal-header.component";
+import { SelectComponent } from "../../select/select.component";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+
+export interface ConfirmModalData {
+  title: string;
+  description: string;
+}
+
+@Component({
+  selector: 'sh-confirm-modal',
+  standalone: true,
+  imports: [
+    ModalButtonsComponent,
+    ModalHeaderComponent,
+    SelectComponent
+  ],
+  templateUrl: './confirm-modal.component.html',
+  styleUrl: './confirm-modal.component.scss'
+})
+export class ConfirmModalComponent {
+  public title: string;
+  public description: string;
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ConfirmModalData,
+  ) {
+    this.title = this.data.title;
+    this.description = this.data.description;
+  }
+
+  public close(value?: boolean) {
+    this.dialogRef.close(value);
+  }
+
+  public handleButtonClicked($event: boolean): void {
+    $event ? this.close(true) : this.dialogRef.close();
+  }
+}
